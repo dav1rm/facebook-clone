8e3e558dcf4e206c07959a08bb86b2ff8a0dda30
Davi Rodrigues <davi.rodrigues@liberedu.com>
2020-02-24 17:40:28 -0300

    Updates

---
+++ precache-manifest.dd7e5ac9e0cf1ca06c0d841cdad8a1b6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fac63c9bb6d3f1261c6f033fe6944b",
+    "url": "/facebook-clone/index.html"
+  },
+  {
+    "revision": "2df16e25aab14a1c26e1",
+    "url": "/facebook-clone/static/js/2.5af842a4.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/facebook-clone/static/js/2.5af842a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd27670aeb355662b0b",
+    "url": "/facebook-clone/static/js/main.dae61b13.chunk.js"
+  },
+  {
+    "revision": "fbdc1aa93da974c934e5",
+    "url": "/facebook-clone/static/js/runtime-main.1f78cad9.js"
+  },
+  {
+    "revision": "6acc1133c30c2501d08a22615676760c",
+    "url": "/facebook-clone/static/media/comment.6acc1133.svg"
+  },
+  {
+    "revision": "0264e1f3a26a7bc2b2df684a06471df2",
+    "url": "/facebook-clone/static/media/grr.0264e1f3.gif"
+  },
+  {
+    "revision": "239d3d02550bd5d0223a4b6d974361fa",
+    "url": "/facebook-clone/static/media/grr_static.239d3d02.png"
+  },
+  {
+    "revision": "d8d20cacb6bbab963a55c7f79826ec8a",
+    "url": "/facebook-clone/static/media/haha.d8d20cac.gif"
+  },
+  {
+    "revision": "66f1d9e12cc345905e284b0202526005",
+    "url": "/facebook-clone/static/media/haha_static.66f1d9e1.png"
+  },
+  {
+    "revision": "7f96bd93c1f0113f568dec1152718f9f",
+    "url": "/facebook-clone/static/media/like.7f96bd93.gif"
+  },
+  {
+    "revision": "ba3f89778a4aaa3a94aa4804a508b609",
+    "url": "/facebook-clone/static/media/like.ba3f8977.svg"
+  },
+  {
+    "revision": "4b916ec17e3e2933fe4e64f6d5720041",
+    "url": "/facebook-clone/static/media/like_static.4b916ec1.png"
+  },
+  {
+    "revision": "9e1dd43a33282fe97ad0b38057fc4f8d",
+    "url": "/facebook-clone/static/media/love.9e1dd43a.gif"
+  },
+  {
+    "revision": "47115a63c8cb0ee26bc12d365e62698f",
+    "url": "/facebook-clone/static/media/love_static.47115a63.png"
+  },
+  {
+    "revision": "a345b96e5490b67b17c8fb6301978251",
+    "url": "/facebook-clone/static/media/sad.a345b96e.gif"
+  },
+  {
+    "revision": "7537d1ed490c4a1e86f8e43d637a61bc",
+    "url": "/facebook-clone/static/media/sad_static.7537d1ed.png"
+  },
+  {
+    "revision": "d21ee90189d34d50f713519542d6fb5a",
+    "url": "/facebook-clone/static/media/share.d21ee901.svg"
+  },
+  {
+    "revision": "6ff3e22dd73579982e7b6f814fec5198",
+    "url": "/facebook-clone/static/media/uau.6ff3e22d.gif"
+  },
+  {
+    "revision": "530a103ee8bb60fc741388f5adbbc906",
+    "url": "/facebook-clone/static/media/uau_static.530a103e.png"
+  }
+]);
