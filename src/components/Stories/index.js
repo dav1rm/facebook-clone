ec6d3e07757e00cc8ae5e838b2257b6dc6102d64
Davi Rodrigues <davi.rodrigues@liberedu.com>
2020-02-24 12:17:45 -0300

    alterando fotos de avatar dos stories

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -13,15 +13,15 @@ export default function Stories() {
       <div className="stories">
         <Story image="https://media-exp1.licdn.com/dms/image/C4D03AQF5ofNur1bmiw/profile-displayphoto-shrink_100_100/0?e=1586995200&v=beta&t=rlQZEAEgdomCpLkqroaqmYCZlOiDZDWMmGF3wulKRaw" />
         <Story
-          avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTKM9BUy0eVYn8_C3sg0J40Oa5MlWpFbS83fleauNdF4W5HQQJQ"
+          avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbg4n40okGbHtt3E5fdzEK-xBLbBUk54tTbyKxz3cZlKupKk0s"
           image="https://cdn.pixabay.com/photo/2013/07/21/13/00/rose-165819__340.jpg"
         />
         <Story
-          avatar="https://lh3.googleusercontent.com/proxy/YHWdGdLFEeXVphbLoLbB8DoGkzehkpwbj1mUqThx0TmHW08UbZoG6S-bV9-xWrqWeyIi0NVxmxm6HesSWEcnePeBIe-fvherihCjIpZULm9WUhuRKGc-hv6HetA"
+          avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT3CgTTxDBkArxvYT_Bta7VsQNDWu2ZXtIQCqFWWJZrQsn05flF"
           image="https://i.pinimg.com/originals/ca/76/0b/ca760b70976b52578da88e06973af542.jpg"
         />
         <Story
-          avatar="https://www.irreverentgent.com/wp-content/uploads/2018/03/Awesome-Profile-Pictures-for-Guys-look-away2.jpg"
+          avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSqVnkZbMWDWOtZq6nPpEJf-EHmzoK5Bb2nWD-4M3NQ-InAFmar"
           image="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500"
         />
       </div>
